2eff95b39e9894c2caff901e53000a917f350516
daverodal <dave.rodal@gmail.com>
2020-04-17 17:17:43 -0400

    added doitUnit to output. and bumped up version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import {playAudio, playAudioBuzz, playAudioLow, counterClick, mapClick, doitOption, doitNext, nextPhaseMouseDown, doitKeypress, showCrtTable, fixItAll, doitSaveGame, rotateUnits, toggleFullScreen, doitCRT} from "./global-funcs";
+import {playAudio, playAudioBuzz, playAudioLow, counterClick, mapClick, doitUnit, doitOption, doitNext, nextPhaseMouseDown, doitKeypress, showCrtTable, fixItAll, doitSaveGame, rotateUnits, toggleFullScreen, doitCRT} from "./global-funcs";
 import {clickBack, phaseBack, playerTurnBack, clickSurge,phaseSurge, playerTurnSurge, timeLive, timeBranch} from "./time-funcs";
 import {syncObj} from "./syncObj";
 import fixHeader from "./fix-header";
 export {fixHeader};
-export {playAudio, playAudioBuzz, playAudioLow, counterClick, mapClick, doitOption, doitNext, nextPhaseMouseDown, doitKeypress, showCrtTable, fixItAll, doitSaveGame, rotateUnits, toggleFullScreen, doitCRT} from "./global-funcs";
-export {clickBack, phaseBack, playerTurnBack, clickSurge,phaseSurge, playerTurnSurge, timeLive, timeBranch} from "./time-funcs";
+export {playAudio, playAudioBuzz, playAudioLow, counterClick, mapClick, doitUnit, doitOption, doitNext, nextPhaseMouseDown, doitKeypress, showCrtTable, fixItAll, doitSaveGame, rotateUnits, toggleFullScreen, doitCRT};
+export {clickBack, phaseBack, playerTurnBack, clickSurge,phaseSurge, playerTurnSurge, timeLive, timeBranch};
 export {syncObj};
 import { doPostRequest } from "./doPostRequest";
 export {doPostRequest};
